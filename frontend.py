2881fe29236f9b969eb04c41c113a8b9cccc592e
anonyxbiz <anonyxbiz@gmail.com>
2024-05-11 23:38:16 +0300

    update frontend.py

--- frontend.py
+++ frontend.py
@@ -12,21 +12,26 @@ class Dl_app:
     async def dler(self, request, response):
         try:
             if request.method == "GET":
-                link = request.query.link or 'None'
+                incoming = dict(request.query.decode())
+                
             elif request.method == "POST": 
-                incoming = self.comps.get_json(request )
-                link = incoming['link'] or 'None'
-            
-            visits = await self.test.check(request.remote_addr)
-            
-            data = {"detail": link}
-            data.update(visits)
+                incoming = self.comps.get_json(request)
+            if incoming:
+                link = incoming["link"] or "None"
+                test = incoming["test"] or "None"
+                
+                data = {"detail": [{"link": link, "test": test}]}
+                
+                async with self.test.check(request.remote_addr) as visits:
+                    data.update(visits)
+            else:
+                abort(403, "Something wen't wrong on our end")
         except Exception as e:
             p(e)
             await Discord().logger(f'Application log: {e}')
-            
+            abort(403, "Something wen't wrong on our end")
+     
         await self.set_headers.verify_request(request, response, do='headers only')
-        
         return data
         
 if __name__ == '__main__':
