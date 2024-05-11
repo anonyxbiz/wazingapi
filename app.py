0691982d8934f65c5ccdb5d30aa5d5ec5f396bc0
anonyxbiz <anonyxbiz@gmail.com>
2024-05-11 21:07:56 +0300

    update

---
+++ app.py
@@ -0,0 +1,67 @@
+# app.py
+from initialize import*
+from frontend import Dl_app
+from apps import Pages, Discord, Obsfucation
+
+app = Bottle()
+pages = Pages()
+logman = Discord()
+testing = Dl_app()
+
+@app.route('/static/<filename:path>')
+def static(filename):
+    good_files = ['css/style.css', 'js/script.js', 'images/favicon.png']
+    if filename == 'js/script.js':
+        abort(404, 'Not found')
+        
+    try:
+        return static_file(filename, root='./static')
+    except Exception as e:
+        a.run(Discord().logger(f'Application log: {e}'))
+        abort(403, e)
+
+@app.route('/')
+def index():
+    try:
+        webpage = a.run(pages.page_manager('index', request, r))
+    except Exception as e:
+        a.run(Discord().logger(f'Application log: {e}'))
+    return webpage
+
+@app.post('/api/dler')
+def dler_api():
+    data = {'link': 'views'}
+    try:
+        data = a.run(Dl_app().dler(request, r))
+        add_headers = a.run(pages.verify_request(request, r, do='headers_only'))
+    except Exception as e:
+        a.run(Discord().logger(f'Application log: {e}'))
+        
+    return data
+    
+@app.route('api/v1/test', method=['GET', 'POST'])
+def test():
+    a.run(pages.verify_request(request, r, do='headers_only'))
+    
+    return a.run(testing.dler(request, r))
+    
+@app.route('/<page>')
+def controlla(page):
+    webpage = a.run(pages.page_manager(page, request, r))
+    
+    return webpage
+        
+if __name__=="__main__":
+    files = ['min.js', '404.js', 'player.js']
+    for i in files:
+        a.run(Obsfucation().obsfucate_code(i))
+    
+    if not args.thread:
+        from apps import keepmealive
+        url = app_info['url']
+        task_thread = Thread(target=keepmealive, args=(url, url))
+        
+        task_thread.start()
+        
+    run(app=app, host="0.0.0.0", port="8004", debug=True, reloader=True)
+    
---
+++ frontend.py
@@ -0,0 +1,31 @@
+# frontend.py
+from initialize import*
+from apps import Components, Protect, Discord
+from downloader import Downloader
+
+app = Components()
+protect = Protect()
+dl = Downloader()
+
+class Dl_app:
+    def __init__(self):
+        pass
+        
+    async def dler(self, request, response):
+        try:
+            ip = request.remote_addr
+            incoming_data = await app.get_json(request)
+            
+            if incoming_data:
+                link = incoming_data['link']
+            else:
+                link = request.query['link']
+                
+            return {'detail': link}
+        except Exception as e:
+            p(e)
+            await Discord().logger(f'Application log: {e}')
+                
+    
+if __name__ == '__main__':
+    pass
